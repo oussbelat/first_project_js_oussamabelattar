a3a99e066ca76d33f78956643f55f25639c739bd
oussama belattar <belattaroussama3@gmail.com>
2025-05-13 04:50:52 +0100

    sign up

--- public/JS/app.js
+++ public/JS/app.js
@@ -13,4 +13,42 @@ Choose:
         alert('Exit');
         break;
     }
+
+    if (choice === 'sign up') {
+        let name = prompt('Enter your full name:').trim();
+        if (/[\d@#\-\+\*/]/.test(name) || name.split(' ').join('').length < 5) {
+            alert('It must not  numbers or special characters,  5 characters long');
+            continue;
+        }
+        name = name.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+
+        let email = prompt('Enter your email:').trim().toLowerCase();
+        if (email.includes(' ') || email.length < 10 || email.split('@').length !== 2 || users.some(user => user.email === email)) {
+            alert(' duplicate email.  10 characters long no spaces and  one "@".');
+            continue;
+        }
+
+        let age = prompt('Enter your age:').trim();
+        if (!/^\d{1,2}$/.test(age) || age < 1 || age > 99) {
+            alert(' a number between 1 and 99.');
+            continue;
+        }
+
+        let password = prompt('Enter your password:').trim();
+        if (password.includes(' ') || !/[@#\-\+\*/]/.test(password) || password.length < 7) {
+            alert('  7 character long and(@, #, -, +, *, /).');
+            continue;
+        }
+
+        let confirmPassword = prompt('Confirm your password:').trim();
+        if (confirmPassword !== password) {
+            alert('Passwords do not match.');
+            continue;
+        }
+
+        users.push({ name, email, age, password });
+        alert('Sign up successful!');
+        console.log(users);
+    }
+
  }
